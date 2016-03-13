a12a1ec34540127dd5a1c78174b48ab8fe53c083
wencheng.wang <wangwencheng0124@163.com>
2016-03-13 12:33:10 +0800

    scrolltextview

---
+++ mysecurity/src/main/java/mysecurity/wencheng/com/mysecurity/widget/ScrollTextView.java
@@ -0,0 +1,35 @@
+package mysecurity.wencheng.com.mysecurity.widget;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by wang on 2016/3/13.
+ */
+public class ScrollTextView extends TextView {
+    public ScrollTextView(Context context) {
+        super(context);
+    }
+
+    public ScrollTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+    public ScrollTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public ScrollTextView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    public boolean isFocused() {
+        return true;
+    }
+}
